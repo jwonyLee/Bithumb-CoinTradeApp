bfbdd9a39a783abf7af98e9179d73ee45f989619
JAEYOUNGYUN <lastmore9@gmail.com>
2022-03-01 15:15:56 +0900

    feat: CoinListViewController 기본 UI 구현

--- Bithumb-CoinTradeApp/Modules/CoinList/CoinListViewController.swift
+++ Bithumb-CoinTradeApp/Modules/CoinList/CoinListViewController.swift
@@ -18,6 +18,14 @@ protocol CoinListCoordinatable {
 final class CoinListViewController: BaseViewController {
     private var coordinator: CoinListCoordinatable
 
+    let tableView = UITableView().then {
+        $0.register(CoinCell.self, forCellReuseIdentifier: CoinCell.reuseIdentifier)
+        $0.estimatedRowHeight = 80
+        $0.rowHeight = UITableView.automaticDimension
+    }
+    
+    var dataSource: UITableViewDiffableDataSource<Int, String>?
+    
     init(coordinator: CoinListCoordinatable) {
         self.coordinator = coordinator
         super.init(nibName: nil, bundle: nil)
@@ -29,23 +37,55 @@ final class CoinListViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let button = UIButton().then {
-            $0.setTitle("누르면 Pager로 넘어가요", for: .normal)
-            $0.setTitleColor(.systemBlue, for: .normal)
-            $0.sizeToFit()
+    }
+    
+    override func setUI() {
+        view.addSubview(tableView)
+        
+        configureDataSource()
+        
+        var snapShot = NSDiffableDataSourceSnapshot<Int, String>()
+        snapShot.appendSections([0])
+        snapShot.appendItems(["1", "2", "3"])
+        dataSource?.apply(snapShot)
+    }
+    
+    func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { tableView, indexPath, itemIdentifier in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CoinCell.reuseIdentifier, for: indexPath) as? CoinCell else { return UITableViewCell() }
+            
+            cell.configure(
+                icon: nil,
+                coinName: itemIdentifier,
+                price: itemIdentifier,
+                fluctateRate: itemIdentifier,
+                isLiked: false) { [weak self] in
+                    self?.didTapLikeButton(itemIdentifier)
+                }
+            
+            return cell
+        })
+        
+        tableView.dataSource = dataSource
+    }
+    
+    override func setConstraint() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
         }
-
-        button.rx.tap
-            .subscribe(with: self) { owner, _ in
-                owner.coordinator.showPager()
-            }
-            .disposed(by: disposeBag)
-        view.addSubview(button)
-
-        button.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.width.height.equalTo(200)
-        }
+    }
+    
+    override func bind() {
+        
+    }
+    
+    override func subscribeUI() {
+        
+    }
+    
+    func didTapLikeButton(_ id: String) {
+        print("like button tapped", id)
     }
 }
