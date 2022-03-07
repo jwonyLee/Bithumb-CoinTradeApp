3318d380a45a12c27420c8d349425e1c71202007
JiWon Lee <jiwon.swdev@gmail.com>
2022-03-07 20:46:19 +0900

    feat: Candlestick API 구현

---
+++ Bithumb-CoinTradeApp/Models/Candlestick.swift
@@ -0,0 +1,46 @@
+//
+//  Candlestick.swift
+//  Bithumb-CoinTradeApp
+//
+//  Created by 이지원 on 2022/03/05.
+//
+
+import Foundation
+
+struct Candlestick: Codable {
+    let status: String
+    let data: [[CandlestickData]]
+}
+
+enum CandlestickData: Codable {
+    case integer(Int)
+    case string(String)
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let x = try? container.decode(Int.self) {
+            self = .integer(x)
+            return
+        }
+        if let x = try? container.decode(String.self) {
+            self = .string(x)
+            return
+        }
+        throw DecodingError.typeMismatch(
+            CandlestickData.self,
+            DecodingError.Context(
+                codingPath: decoder.codingPath,
+                debugDescription: "Wrong type for Datum"
+            ))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .integer(let x):
+            try container.encode(x)
+        case .string(let x):
+            try container.encode(x)
+        }
+    }
+}
